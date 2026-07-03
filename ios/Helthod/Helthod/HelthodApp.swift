16fe157c09c461bf4989c8f6a228a31595aeb062
wfsda1 <gamamil.com@gmail.com>
2026-07-03 13:46:22 +0700

    feat: init ios

---
+++ ios/Helthod/Helthod/HelthodApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelthodApp.swift
+//  Helthod
+//
+//  Created by Сергей Лихачев on 03.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct HelthodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
