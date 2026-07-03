b1c2940eb1be5f41419f166809d9c955ae65a303
Andrel0 <andrey171726@gmail.com>
2026-07-03 15:45:40 +0700

    setup backend with prisma postgres and docker

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.send("Backend works!");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
